7e94053e1244fd2c65333d42ce316b0158fafb44
Blang <shyboy0113@gmail.com>
2023-07-11 14:17:48 +0900

    2023-07-11 오후 2시. 싱글톤 패턴 제네릭 상속까지 구현 완료

--- Test4/Assets/Script/GameManager.cs
+++ Test4/Assets/Script/GameManager.cs
@@ -4,43 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
+    public int score;
 
-    private static GameManager instance = null;
-
-    private void Awake()
+    private void Start()
     {
-
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
+        // 게임 매니저의 인스턴스에 접근하여 변수를 사용할 수 있습니다.
+        score = 0;
     }
 
-    public static GameManager Instance
+    public void IncreaseScore(int points)
     {
-        get
-        {
-            if (instance == null) return null;
-            return instance;
-        }
-    }
-
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        score += points;
     }
 }
---
+++ Test4/Assets/Script/Singleton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance; //지역 변수이므로 _추가함
+    private static object lockObject = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (lockObject)
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject(typeof(T).Name);
+                        _instance = singletonObject.AddComponent<T>();
+                        DontDestroyOnLoad(singletonObject);
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
