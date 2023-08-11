0751188f906e5799cc5579b1a327a521ccdd1812
Blang <shyboy0113@gmail.com>
2023-08-11 16:33:53 +0900

    주인공 이미지 조금 추가했음. 나머지 코드 파일정리

--- Test4/Assets/Script/Hero/Move.cs
+++ Test4/Assets/Script/Hero/Move.cs
@@ -8,17 +8,25 @@ public class Move : MonoBehaviour
     public CharacterController2D controller;
     
     public float speed = 40f;
-
     private float inputX = 0f;
+    
+    private bool jump = false;
+    private bool crouch = false;
     
     private void Update()
     {
         inputX = Input.GetAxisRaw("Horizontal") * speed;
 
+        if (Input.GetButtonDown("Jump")) jump = true;
+        if (Input.GetButtonDown("Crouch")) crouch = true;
+        else if(Input.GetButtonUp("Crouch")) crouch = false;
+        
     }
 
     private void FixedUpdate()
     {
-        controller.Move(inputX * Time.fixedDeltaTime, false, false);
+        controller.Move(inputX * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+        
     }
 }
