4be9624e633eaf01ec067adf7c1ed29a65e54f13
Hong <shyboy0113@gmail.com>
2023-09-19 22:00:40 +0900

    주인공 애니메이션 추가. 아직 OnLandEvent 로직 제대로 확인 안 됐음

--- Test4/Assets/Script/Hero/Move.cs
+++ Test4/Assets/Script/Hero/Move.cs
@@ -2,22 +2,34 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Move : MonoBehaviour
 {
     public CharacterController2D controller;
+    private Animator _animator;
     
     public float speed = 40f;
     private float inputX = 0f;
     
     private bool jump = false;
     private bool crouch = false;
-    
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         inputX = Input.GetAxisRaw("Horizontal") * speed;
 
-        if (Input.GetButtonDown("Jump")) jump = true;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            _animator.Play("Player_Fall");
+               
+        }
         if (Input.GetButtonDown("Crouch")) crouch = true;
         else if(Input.GetButtonUp("Crouch")) crouch = false;
         
