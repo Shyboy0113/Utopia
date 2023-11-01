09c0fd173224e9984fba0cb771154e1ff69f0bd3
Hong <shyboy0113@gmail.com>
2023-11-01 17:31:27 +0900

    튜토리얼 창에 선/악 게이지 선택 별 열 수 있는 상자 추가
    겁쟁이 상자가 추가되었다!

--- Test4/Assets/Script/StreetLightMan.cs
+++ Test4/Assets/Script/StreetLightMan.cs
@@ -1,15 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using PixelCrushers.DialogueSystem;
 
 public class StreetLightMan : MonoBehaviour
 {
     public Transform transform;
+
+    public TMP_Text _text;
+
+    private void Update()
+    {
+        _text.text = DialogueLua.GetVariable("Evil Gage").asString;
+    }
 
     public void ChangeRotate()
     {
         transform.rotation = Quaternion.Euler(0, 0, -90);
         transform.position += new Vector3(1,-0.8f,0);
     }
+    
+    
 
 }
