7cc136f978234f80695a4f8df8677e9973ea4920
Hong <shyboy0113@gmail.com>
2023-11-04 16:25:52 +0900

    게임 튜토리얼 화면 버그 수정

--- Test4/Assets/Title/Title_Button.cs
+++ Test4/Assets/Title/Title_Button.cs
@@ -1,13 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using PixelCrushers.DialogueSystem;
 
 public class Title_Button : MonoBehaviour
 {
+    public UnityEngine.UI.Toggle skipTutorialToggle;
+    public GameObject skipCanvas;
+
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
     public void NewGame()
     {
-        string targetSceneName = "Test"; // Replace "TargetScene" with the name of your target scene
-        LoadingSceneController.LoadScene(targetSceneName);
+        Debug.Log(PlayerPrefs.HasKey("SkipTutorial"));
+        
+        if (!PlayerPrefs.HasKey("SkipTutorial") || PlayerPrefs.GetInt("SkipTutorial") == 0)
+        {
+            skipCanvas.SetActive(true);   
+        }
+        else
+        {
+            PixelCrushers.SaveSystem.LoadScene("Tutorial");
+        }
+
+    }
+    public void ChangeSkipPrefs()
+    {
+        // Save the toggle state to PlayerPrefs
+        PlayerPrefs.SetInt("SkipTutorial", skipTutorialToggle.isOn ? 1 : 0);
+        PlayerPrefs.Save();
+        
+    }
+    public void GotoManipulationTutorial()
+    {
+        PixelCrushers.SaveSystem.LoadScene("Manipulation Tutorial");
+    }
+    
+    public void SkipToTutorial()
+    {
+        PixelCrushers.SaveSystem.LoadScene("Tutorial");
     }
 
     public void LoadGame()
